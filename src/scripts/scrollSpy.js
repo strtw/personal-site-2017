8e0ee963a99deb7e50e8a078ac9f1da13cabcda6
strtw <souporstu@gmail.com>
2017-11-02 09:44:33 -0700

    updated tabs in music catalog tab

--- src/scripts/scrollSpy.js
+++ src/scripts/scrollSpy.js
@@ -4,30 +4,27 @@
 
 (function(){
     'use strict';
-    var desktopScroll = window.matchMedia( "(min-width: 800px)" );
+    const desktopScroll = window.matchMedia( "(min-width: 800px)" );
 
     function scrollSpy(){
         window.addEventListener("scroll", function scrollSpy() {
-            var i;
-            var j;
             var offsetHeight = 200;
-            var classCount = 0;
             var navLinkHash;
             var dataItemId;
-            var dataItems = document.querySelectorAll(".content__section");
-            var navLinks = document.querySelectorAll(".sideBar__list a");
-            var navBullets = document.querySelectorAll(".sideBar__item");
-            for ( j = 0; j < dataItems.length; j++) {
+            const dataItems = document.querySelectorAll(".content__section");
+            const navLinks = document.querySelectorAll(".sideBar__list a");
+            const navBullets = document.querySelectorAll(".sideBar__item");
+            for ( let j = 0; j < dataItems.length; j++) {
                 offsetHeight = dataItems[j].offsetHeight;
                 if (dataItems[j].getBoundingClientRect().top <= offsetHeight && dataItems[j].getBoundingClientRect().top <= offsetHeight) {
-                    for (i = 0; i < navLinks.length; i++) {
+                    for (let i = 0; i < navLinks.length; i++) {
                         if(navBullets[i].classList.contains('sideBar--scrollSpyActive')){
                             navBullets[i].classList.remove('sideBar--scrollSpyActive');
                         }
                         navLinkHash = navLinks[i].hash;
                         if (navLinkHash !== "") {
                             dataItemId = dataItems[j].getAttribute("id");
-                            if (navLinks[i].hash == "#" + dataItemId && classCount < 1) {
+                            if (navLinks[i].hash === "#" + dataItemId) {
                                 navBullets[i].classList.add('sideBar--scrollSpyActive');
                             }
                         }
